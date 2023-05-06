25f0e6219e580bbee5d9c868f929ecb07251f93a
nhvn <alannhan@ymail.com>
2023-05-06 15:02:53 -0700

    finally added night mode but now I need to tweak the night changes to look better

--- my-portfolio/js/script.js
+++ my-portfolio/js/script.js
@@ -1,32 +1,42 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Get references to DOM elements
-    const body = document.body;
-    const nightModeToggle = document.getElementById("nightModeToggle");
-    const navLinks = document.querySelectorAll('nav a[href^="#"]');
-    const footerText = document.querySelector('footer p');
-  
-    // Night mode toggle
-    nightModeToggle.addEventListener("click", function() {
-      body.classList.toggle("night-mode");
-      nightModeToggle.classList.toggle("active");
-    });
-  
-    // Smooth scrolling
-    navLinks.forEach(link => {
-      link.addEventListener('click', function(e) {
-        e.preventDefault();
-        const target = document.querySelector(this.getAttribute('href'));
-        const navHeight = document.querySelector('nav').offsetHeight;
-        const scrollToPosition = target.offsetTop - navHeight;
-        window.scrollTo({
-          top: scrollToPosition,
-          behavior: 'smooth'
-        });
-      });
-    });
-  
-    // Add current year to footer
-    const currentYear = new Date().getFullYear();
-    footerText.textContent += ` | ${currentYear}`;
+$(document).ready(function() {
+  // Get references to DOM elements
+  const body = $('body');
+  const nav = $('nav');
+  const socialIcons = $('.social-icons');
+  const nightModeToggle = $('#nightModeToggle');
+  const navLinks = $('nav a[href^="#"]');
+  const footerText = $('footer p');
+  const nightModeElements = [body, nav, socialIcons];
+
+  // Night Mode
+  let nightModeButton = $('<i class="fas fa-moon night-mode-icon"></i>');
+  nightModeButton.click(function() {
+    toggleNightMode();
   });
-  
+  body.append(nightModeButton);
+
+  function toggleNightMode() {
+    nightModeElements.forEach(element => element.toggleClass("night-mode"));
+    nightModeButton.toggleClass("active");
+    localStorage.setItem("nightMode", body.hasClass("night-mode"));
+  }
+
+  if (localStorage.getItem("nightMode") === "true") {
+    toggleNightMode();
+  }
+
+  // Smooth scrolling
+  navLinks.on('click', function(e) {
+    e.preventDefault();
+    const target = $($(this).attr('href'));
+    const navHeight = nav.outerHeight();
+    const scrollToPosition = target.offset().top - navHeight;
+    $('html, body').animate({
+      scrollTop: scrollToPosition
+    }, 1000);
+  });
+
+  // Add current year to footer
+  const currentYear = new Date().getFullYear();
+  footerText.text(footerText.text() + ` | ${currentYear}`);
+});
